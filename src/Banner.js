73650027aa160fcf13586152635531ce5d851655
Rajat Wankhede <rajatwankhede09@gmail.com>
2021-03-15 16:02:02 +0530

    carousel added in place of banner

--- src/Banner.js
+++ src/Banner.js
@@ -2,32 +2,51 @@ import React, { useEffect, useState } from 'react';
 import './Banner.css';
 import axios from './axios';
 import requests from './Requests';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Carousel from 'react-bootstrap/Carousel';
 
 
 function Banner() {
 
     const [movie, setMovies] = useState([]);
     const baseurl_image = 'https://image.tmdb.org/t/p/original/';
+    movie.length = 12;
 
 
     useEffect(() => {
         async function fetchData() {
             const request = await axios.get(requests.fetchAmazonOriginals);
-            setMovies(request.data.results[Math.floor(Math.random() * request.data.results.length)]);
+            setMovies(request.data.results);
             return request
         }
         fetchData();
     }, []);
-    return (
-        <header className="banner" style={{
-            backgroundImage: `url('https://image.tmdb.org/t/p/original${movie?.backdrop_path}')`,
-            backgroundSize: 'cover',
-            backgroundPosition: 'center center'
 
-        }}>
-            
+    const truncate = (string, n) => {
+        return string?.length > n ? string.substr(0, n-1) + "..." : string;
+    }
+    return (
+        <header className="banner">
+            <Carousel>
+                {movie.map(m => (
+                    <Carousel.Item>
+                        <img
+                        className="d-block w-100"
+                        src={`${baseurl_image}${m?.backdrop_path}`}
+                        alt="First slide"
+                        />
+                        <Carousel.Caption>
+                            <h3>{m?.title}</h3>
+                            <p>{truncate(`${m?.overview}`, 120)}</p>
+                        </Carousel.Caption>
+                    </Carousel.Item>
+                ))}
+                
+                
+            </Carousel> 
+        
         </header>
     )
 }
 
-export default Banner
+export default Banner;
--- src/Nav.js
+++ src/Nav.js
@@ -5,10 +5,10 @@ function Nav() {
     return (
         <div className="nav">
             <img className="nav__logo" src="https://www.jamesdodkins.com/uploads/6/4/7/0/6470224/published/prime-video_1.png?1552987507" alt="amazon-logo"/>
-            <li>Home</li>
-            <li>TV</li>
-            <li>Movies</li>
-            <li>Kids</li>
+            <li className="bb">Home</li>
+            <li className="bb">TV</li>
+            <li className="bb">Movies</li>
+            <li className="bb">Kids</li>
             <input className="search__bar" type="text" placeholder="Search" />
             <img className="nav__avatar" src="https://m.media-amazon.com/images/G/02/CerberusPrimeVideo-FN38FSBD/adult-1.png" alt="avatar-logo"/>
         </div>
